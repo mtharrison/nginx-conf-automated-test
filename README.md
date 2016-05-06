5bbc7b9869edcd351018b3b47c0bf17da8bab327
Matt Harrison <hi@matt-harrison.com>
2016-05-06 20:45:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Automated nginx config test using docker
+
+## Setup
+
+1. Have docker installed
+2. Have the `nginx` image around
+3. `npm install` in this project
+4. `npm test`
+
+Expected output:
+
+![output](https://cldup.com/YJKZIuTvy5.png)
---
+++ test/index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// Load modules
+
+const ChildProcess = require('child_process');
+const Code = require('code');
+const Lab = require('lab');
+const Path = require('path');
+const Wreck = require('wreck');
+
+
+// Declare internals
+
+const internals = {};
+
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const describe = lab.experiment;
+const expect = Code.expect;
+const it = lab.test;
+const afterEach = lab.afterEach;
+
+
+const dockerHost = '192.168.99.100';     // yours may vary
+const port = '4321';                     // pick a free port
+let proc;
+
+
+// Starts a nginx container running with my config
+// calls-back when ready
+
+const setup = function (configFile, callback) {
+
+    const basedir = Path.join(__dirname, '..');
+    proc = ChildProcess.spawn('docker', ['run', '-v', `${basedir}/public:/var/www`, '-v', `${basedir}/${configFile}:/etc/nginx/nginx.conf`, '-p', `${port}:${port}`, 'nginx']);
+    setTimeout(callback, 1000);
+};
+
+afterEach((done) => {
+
+    proc.kill();
+    done();
+});
+
+describe('nginx conf', () => {
+
+    it('works with good config', (done) => {
+
+        setup('nginx-good.conf', (err, cleanup) => {
+
+            if (err) {
+                throw err;
+            }
+
+            Wreck.get(`http://${dockerHost}:${port}/`, (err, res, payload) => {
+
+                if (err) {
+                    throw err;
+                }
+
+                expect(res.headers['x-dessert-header']).to.equal('frozen yogurt');
+                expect(payload.toString()).to.equal('Hello world!\n');
+                done();
+            });
+        });
+    });
+
+    it('works with bad config', (done) => {
+
+        setup('nginx-bad.conf', (err, cleanup) => {
+
+            if (err) {
+                throw err;
+            }
+
+            Wreck.get(`http://${dockerHost}:${port}/`, (err, res, payload) => {
+
+                if (err) {
+                    throw err;
+                }
+
+                expect(res.headers['x-dessert-header']).to.equal('frozen yogurt');
+                expect(payload.toString()).to.equal('Hello world!\n');
+                done();
+            });
+        });
+    });
+});
